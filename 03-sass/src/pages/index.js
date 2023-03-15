c511b847536f27af4fc15900da165aee656b29bf
carlos <crdgom@gmail.com>
2023-03-14 20:40:06 -0600

    Base project config

---
+++ 03-sass/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home'
+import Login from './Login'
+import SignUp from './SignUp'
+import DashBoard from './DashBoard'
+import Secret from './Secret'
+
+export { Home, Login, SignUp, DashBoard, Secret }
