802875d136074ee0ac25777ebdfaed6f5c6a056d
carlos <crdgom@gmail.com>
2023-03-21 20:52:40 -0600

    ruta dashbor protegida con redireccion

--- 03-sass/src/routes/index.jsx
+++ 03-sass/src/routes/index.jsx
@@ -1,11 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from '@/context/AuthContext'
 import { DashBoard, Home, Login, Secret, SignUp } from '@/pages'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<DashBoard />} />
+      <Route
+        path='/dashboard'
+        element={
+          isAuth
+            ? <DashBoard />
+            : <Navigate to='/login' replace />
+        }
+      />
       <Route path='/login' element={<Login />} />
       <Route path='/secret' element={<Secret />} />
       <Route path='/signup' element={<SignUp />} />
